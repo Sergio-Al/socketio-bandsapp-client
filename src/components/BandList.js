733d04636aae7ff7b46e8e3885caba04beb4ceca
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-03-25 22:00:02 -0400

    Updating name data
    We are using input events to change our name
    -onChange will update our band name
    -onBlur is triggered when the user left to focus the current input, in this function we will trigger (emit)  the socket event that the server will listen of.

--- src/components/BandList.js
+++ src/components/BandList.js
@@ -7,6 +7,26 @@ const BandList = ({ data }) => {
     setBands(data);
   }, [data]);
 
+  const updateName = (event, id) => {
+    const newName = event.target.value;
+
+    setBands((bands) =>
+      bands.map((band) => {
+        if (band.id === id) {
+          band.name = newName;
+        }
+
+        return band;
+      })
+    );
+  };
+
+  const onLostFocus = (id, nombre) => {
+    console.log(id, nombre);
+
+    // TODO: triggers the socket events
+  };
+
   const createRows = () => {
     return bands.map((band) => (
       <tr key={band.id}>
@@ -14,10 +34,15 @@ const BandList = ({ data }) => {
           <button className="btn btn-primary"> +1 </button>
         </td>
         <td>
-          <input className="form-control" value={band.name} />
+          <input
+            className="form-control"
+            value={band.name}
+            onChange={(event) => updateName(event, band.id)}
+            onBlur={() => onLostFocus(band.id, band.name)}
+          />
         </td>
         <td>
-          <h3>14</h3>
+          <h3>{band.votes}</h3>
         </td>
         <td>
           <button className="btn btn-danger">Delete</button>
